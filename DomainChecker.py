471338b41bd2b636ccff019c8ef20c1fcbf7dd20
Stian <79229317+0xStian@users.noreply.github.com>
2022-09-18 01:19:24 +0200

    Update DomainChecker.py

--- DomainChecker.py
+++ DomainChecker.py
@@ -1,14 +1,17 @@
 import whois    # pip install python-whois
 from random import choice
 
-available_domains = "available_domains.txt"
-show_unavailable_domains = True
-letters = 4
+available_domains = "available_domains.txt"                 # what file to save available domains to
+show_unavailable_domains = True                             # print unavailable domains to console
+letters = 4                                                 # how many letters/numbers in generated url
+TDL = ".com"                                                # what TDL to use
+letters_to_include = "1234567890qwertyuioplkjhgfdsazxcvbnm" # what letters and numbers will be included in the randomly generated url
 
-def get_random_string(size=letters, chars="1234567890qwertyuioplkjhgfdsazxcvbnm"):
+# generate the random domain
+def get_random_string(size=letters, chars=letters_to_include):
     return ''.join(choice(chars) for _ in range(size))
 
-
+# check if domain is available
 def send_request(domain):
     try:
         get_info = whois.whois(domain)
@@ -18,7 +21,7 @@ def send_request(domain):
 
 
 while True:
-    current_domain = str(get_random_string() + ".com")
+    current_domain = str(get_random_string() + TDL)
 
     if send_request(current_domain) is True:
         print(f"X | {current_domain} | is available")
